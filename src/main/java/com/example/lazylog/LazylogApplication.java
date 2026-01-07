4ca1a45f5a2587f63d937321c79ee8060c61d9bf
Chan Weng Ho <cwengho@yahoo.com>
2026-01-07 20:48:17 +0800

    initialize Spring project

---
+++ src/main/java/com/example/lazylog/LazylogApplication.java
@@ -0,0 +1,13 @@
+package com.example.lazylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazylogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazylogApplication.class, args);
+	}
+
+}
